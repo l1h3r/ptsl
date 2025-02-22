2331ab250c796b256eeefbd50a17843e236e7a74
l1h3r <5410284+l1h3r@users.noreply.github.com>
2025-02-22 11:04:59 +0400

    Include generated protobuf types

---
+++ crates/ptsl-protos/src/lib.rs
@@ -0,0 +1,9 @@
+//! PTSL Protobuf Definitions
+
+pub mod types {
+  //! Compiled protobuf types.
+  #![allow(deprecated)]
+  #![allow(clippy::deprecated_semver)]
+  #![allow(clippy::tabs_in_doc_comments)]
+  tonic::include_proto!("ptsl");
+}
